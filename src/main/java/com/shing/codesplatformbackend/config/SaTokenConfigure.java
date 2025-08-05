e30aaf08aba6c987b71ccf7dcad51b328aecd7d3
LBC <541014133@qq.com>
2025-08-05 12:02:37 +0800

    config: 更新 Sa-Token 拦截器配置
    - 添加放行地址

--- src/main/java/com/shing/codesplatformbackend/config/SaTokenConfigure.java
+++ src/main/java/com/shing/codesplatformbackend/config/SaTokenConfigure.java
@@ -20,14 +20,17 @@ public class SaTokenConfigure implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册 Sa-Token 登录拦截器，默认拦截所有接口
         registry.addInterceptor(new SaInterceptor(handler -> StpUtil.checkLogin()))
-                .addPathPatterns("/**") // 注意：会自动加上 context-path，即 /api/**
+                .addPathPatterns("/**")
                 .excludePathPatterns(
-                        "/api/user/login",      // 登录接口
-                        "/api/user/register",   // 注册接口
-                        "/api/user/code",       // 获取验证码（如图形验证码、邮箱验证码）
-                        "/api/common/**",       // 公共接口（如AI模型列表、公告等）
-                        "/favicon.ico",         // 避免浏览器请求干扰
-                        "/error"                // Spring Boot 默认错误处理
+                        // knife4j文档地址
+                        "/doc.html",
+                        // swagger-ui, 没必要放开
+                        "/swagger-ui/**",
+                        // webjars
+                        "/webjars/**",
+                        // api
+                        "/v3/api-docs/**",
+                        "/error"
                 );
     }
 
