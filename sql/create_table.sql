ab43409754f5113c3018628da1ffc213eecf58da
LBC <541014133@qq.com>
2025-08-05 23:13:11 +0800

    feat(user): 添加用户扩展属性- 增加会员过期时间、会员兑换码、会员编号等字段 - 添加分享码和邀请用户 id 字段

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -20,6 +20,12 @@ create table if not exists user
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
+    /*扩展*/
+    vipExpireTime datetime     null comment '会员过期时间',
+    vipCode       varchar(128) null comment '会员兑换码',
+    vipNumber     bigint       null comment '会员编号',
+    shareCode     varchar(20)  DEFAULT NULL COMMENT '分享码',
+    inviteUser    bigint       DEFAULT NULL COMMENT '邀请用户 id',
     UNIQUE KEY uk_userAccount (userAccount),
     INDEX idx_userName (userName)
-    ) comment '用户' collate = utf8mb4_unicode_ci;
+) comment '用户' collate = utf8mb4_unicode_ci;
