6c26631aeb82680c9f3cc27f2018fbc8fc2ef877
LBC <541014133@qq.com>
2025-08-11 23:33:45 +0800

    refactor(user): 优化用户相关请求的参数校验
    -为不同操作场景（创建、更新、登录、查询）添加了特定的校验分组
    -统一了校验注解的错误消息格式
    - 在 UserRegisterRequest 中添加了密码复杂度校验

--- src/main/java/com/shing/codesplatformbackend/model/dto/user/UserAddRequest.java
+++ src/main/java/com/shing/codesplatformbackend/model/dto/user/UserAddRequest.java
@@ -1,6 +1,7 @@
 package com.shing.codesplatformbackend.model.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -19,29 +20,33 @@ public class UserAddRequest implements Serializable {
     /**
      * 用户昵称
      */
-    @NotBlank(message = "账号不能为空")
-    @Size(min = 4, message = "账号长度至少4位")
+    @NotBlank(message = "昵称不能为空", groups = ValidationGroups.Create.class)
+    @Size(min = 2, max = 20, message = "昵称长度应为2-20位", groups = ValidationGroups.Create.class)
     private String userName;
 
     /**
      * 账号
      */
-    @NotBlank(message = "昵称不能为空")
+    @NotBlank(message = "账号不能为空", groups = ValidationGroups.Create.class)
+    @Size(min = 4, max = 20, message = "账号长度应为4-20位", groups = ValidationGroups.Create.class)
     private String userAccount;
 
     /**
      * 用户头像
      */
+    @Size(max = 255, message = "头像地址过长", groups = ValidationGroups.Create.class)
     private String userAvatar;
 
     /**
      * 用户简介
      */
+    @Size(max = 500, message = "简介长度不能超过500字符", groups = ValidationGroups.Create.class)
     private String userProfile;
 
     /**
      * 用户角色: user, admin
      */
+    @Pattern(regexp = "user|admin", message = "用户角色只能是 user 或 admin", groups = ValidationGroups.Create.class)
     private String userRole;
 
     @Serial
--- src/main/java/com/shing/codesplatformbackend/model/dto/user/UserQueryRequest.java
+++ src/main/java/com/shing/codesplatformbackend/model/dto/user/UserQueryRequest.java
@@ -1,12 +1,16 @@
 package com.shing.codesplatformbackend.model.dto.user;
 
 import com.shing.codesplatformbackend.common.PageRequest;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * 用户查询请求
+ */
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class UserQueryRequest extends PageRequest implements Serializable {
@@ -19,16 +23,19 @@ public class UserQueryRequest extends PageRequest implements Serializable {
     /**
      * 用户昵称
      */
+    @Size(max = 20, message = "昵称长度不能超过20位", groups = ValidationGroups.Query.class)
     private String userName;
 
     /**
      * 账号
      */
+    @Size(max = 20, message = "账号长度不能超过20位", groups = ValidationGroups.Query.class)
     private String userAccount;
 
     /**
      * 简介
      */
+    @Size(max = 500, message = "简介长度不能超过500字符", groups = ValidationGroups.Query.class)
     private String userProfile;
 
     /**
