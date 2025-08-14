c3c018498cc8582574ac8420aaab7b506c01652f
LBC <541014133@qq.com>
2025-08-14 16:55:16 +0800

    feat(model): 添加代码生成类型枚举
    添加 CodeGenTypeEnum 枚举类，用于定义代码生成的类型：
    - 原生 HTML 模式
    - 原生多文件模式

---
+++ src/main/java/com/shing/codesplatformbackend/model/enums/CodeGenTypeEnum.java
@@ -0,0 +1,43 @@
+package com.shing.codesplatformbackend.model.enums;
+
+import cn.hutool.core.util.ObjectUtil;
+import lombok.Getter;
+
+/**
+ * 代码生成类型枚举
+ *
+ * @author Shing
+ * date 4/8/2025
+ */
+@Getter
+public enum CodeGenTypeEnum {
+
+    HTML("原生 HTML 模式", "html"),
+    MULTI_FILE("原生多文件模式", "multi_file");
+
+    private final String text;
+    private final String value;
+
+    CodeGenTypeEnum(String text, String value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * 根据 value 获取枚举
+     *
+     * @param value 枚举值的value
+     * @return 枚举值
+     */
+    public static CodeGenTypeEnum getEnumByValue(String value) {
+        if (ObjectUtil.isEmpty(value)) {
+            return null;
+        }
+        for (CodeGenTypeEnum anEnum : CodeGenTypeEnum.values()) {
+            if (anEnum.value.equals(value)) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+}
